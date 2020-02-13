93d2081bc3b259bfbe5844e49ed5be46dece119b
Alexander Pershin <AlexandrPershit-058@mail.ru>
2020-02-13 15:43:35 +0400

    create method render in class Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,22 @@
 class Router {
-    
+  constructor(routes) {
+    this._hooks = {};
+    this._routes = routes;
+    this._hash = window.location.hash;
+  }
+
+  _location(hash) {
+    const url = new URL(window.location);
+    url.hash = hash;
+    window.location = url.toString();
+  }
+
+  render(hash) {
+    if(! (hash in this._routes)) return;
+    this._hash = hash;
+    this._location(hash);
+    this._routes[hash].render();
+  }
 }
+
+export default Router;
